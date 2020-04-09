ae95a336cd396bb47e1ca8124d0ced753bd05857
Marco Silvestri <m.silvestri03@gmail.com>
2020-04-09 16:58:37 +0200

    Structure

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    var buttonPrev = $('.app__img').prev();
+    var buttonNext = $('.app__img').next();
+
+    buttonPrev.click(function () { 
+        console.log('sinistra');
+        
+        
+    });
+
+    buttonNext.click(function () { 
+        console.log('destra');
+        
+        
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
