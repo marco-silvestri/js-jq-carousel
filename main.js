2f94ab8f9058ff7d02057245b0e201e5aab9791e
Marco Silvestri <m.silvestri03@gmail.com>
2020-04-09 17:30:08 +0200

    Not working

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 $(document).ready(function () {
     var buttonPrev = $('.app__img').prev();
     var buttonNext = $('.app__img').next();
-
+    var currentImg = $('.app__img').has('active');
+    
     buttonPrev.click(function () { 
-        console.log('sinistra');
+        console.log($(this));
+        currentImg.next().addClass('active');
+        currentImg.removeClass('active');
         
         
     });
 
     buttonNext.click(function () { 
-        console.log('destra');
+        console.log($(this));
+        currentImg.prev().addClass('active');
+        currentImg.removeClass('active');
         
         
     });
 
-
+    currentImg.click(function () { 
+        console.log($(this));
+        
+        
+    });
 
 
 
